b002da1cf467fcd7d181336ef2cb5d8f342d1ca6
Valentin Pico <kwnigpico@gmail.com>
2022-12-15 00:02:54 -0500

    2da practicaa

--- tareafluuter - 2 - Asincronía/asincronia_con_flutter-master/android/app/src/main/kotlin/com/example/asincronia_con_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asincronia_con_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tareafluuter - 2 - Asincronía/asincronia_con_flutter-master/android/app/src/main/kotlin/com/example/asyncronica/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.asyncronica
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
